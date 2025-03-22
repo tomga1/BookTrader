24609a0567a96e1a93155a60b12d55b87ac0ff3d
tomga1 <tomasgarcia003@gmail.com>
2025-03-22 11:53:26 -0300

    primer commit

---
+++ BookTrader/Controllers/CategoriasController.cs
@@ -0,0 +1,28 @@
+﻿using BookTrader.Data;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore.ChangeTracking;
+
+namespace BookTrader.Controllers
+{
+    public class CategoriasController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public CategoriasController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult GetCategorias()
+        {
+            var categorias = _context.Categorias.ToList();
+            return View(categorias);
+        }
+    }
+}
