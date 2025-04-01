1545f56acee955942d04c17a187a2f504eebb33f
tomga1 <tomasgarcia003@gmail.com>
2025-04-01 17:43:08 -0300

    Verificacion de email terminado

--- BookTrader/Controllers/AccountController.cs
+++ BookTrader/Controllers/AccountController.cs
@@ -13,5 +13,10 @@ namespace BookTrader.Controllers
         {
             return View();
         }
+
+        public IActionResult VerifyEmail()
+        {
+            return View();
+        }
     }
 }
