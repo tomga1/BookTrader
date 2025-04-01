e944e159e1260f2ebda6bef008afd024646eeedc
tomga1 <tomasgarcia003@gmail.com>
2025-04-01 16:51:31 -0300

    registro finalizado

--- BookTrader/ViewModels/RegisterViewModel.cs
+++ BookTrader/ViewModels/RegisterViewModel.cs
@@ -12,7 +12,7 @@ namespace BookTrader.ViewModels
         [Required(ErrorMessage = "El Password es requerido")]
         [StringLength(40, MinimumLength = 8, ErrorMessage = "The {0} must be at {2} and at max {1} characters long.")]
         [DataType(DataType.Password)]
-        [Compare("ConfirmarContraseña", ErrorMessage = "La contraseña no coincide.")]
+        [Compare("ConfirmPassword", ErrorMessage = "La contraseña no coincide.")]
         public string Password { get; set; }
 
         [Required(ErrorMessage = "El nombre es requerido")]
