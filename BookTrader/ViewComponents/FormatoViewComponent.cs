f80fbb0f4ded800daceb96c41c2cad1b46fa4c00
tomga1 <tomasgarcia003@gmail.com>
2025-04-14 11:28:17 -0300

    formato ya se guarda con la info del formulario

---
+++ BookTrader/ViewComponents/FormatoViewComponent.cs
@@ -0,0 +1,29 @@
+﻿using BookTrader.Data;
+using BookTrader.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace BookTrader.ViewComponents
+{
+    public class FormatoViewComponent : ViewComponent
+    {
+        private readonly ApplicationDbContext _context;
+
+        public FormatoViewComponent(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<IViewComponentResult> InvokeAsync(bool dropdown = false)
+        {
+            var formato = await _context.Formato
+                .OrderBy(c => c.Nombre)
+                .ToListAsync();
+            ViewData["Dropdown"] = dropdown;
+            return View(formato);
+        }
+
+    }
+}
