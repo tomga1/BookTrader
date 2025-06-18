c91979781fdeafdcbd36dc0c9d82ec5eae3674c4
tomga1 <tomasgarcia003@gmail.com>
2025-06-18 11:56:53 -0300

    migraciones

---
+++ BookTrader/Models/PasswordResetRequest.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BookTrader.Models
+{
+    public class PasswordResetRequest
+    {
+        public int Id { get; set; }
+        [Required]
+        public string Email { get; set; }
+        [Required]
+        public string Token { get; set; }
+        [Required]
+        public Guid GuidCode { get; set; }
+        public DateTime Expiration { get; set; }
+        public bool Used { get; set; }
+
+    }
+}
